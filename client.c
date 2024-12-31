45f27e9c4013c02283db34c81a11ddaeb8e075c0
tarinaihitori <kazuro0530@gmail.com>
2024-12-31 15:04:54 +0900

    add client

---
+++ client.c
@@ -0,0 +1,72 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 8000
+
+int main(int argc, char *argv[]) {
+    int sockfd, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[256];
+
+    if (argc < 3) {
+        fprintf(stderr, "usage %s hostname message\n", argv[0]);
+        exit(0);
+    }
+
+    // ソケットを作成する
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    // サーバーのホスト名を取得する
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+
+    // 接続先アドレスを設定する
+    memset((char *)&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    memcpy((char *)&serv_addr.sin_addr.s_addr, (char *)server->h_addr,
+           server->h_length);
+    serv_addr.sin_port = htons(PORT);
+
+    // サーバーに接続する
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR connecting");
+        exit(1);
+    }
+
+    // データを送信する
+    memset(buffer, 0, 256);
+    strcpy(buffer, argv[2]);
+    n = send(sockfd, buffer, strlen(buffer), 0);
+    if (n < 0) {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+
+    // データを受信する
+    memset(buffer, 0, 256);
+    n = recv(sockfd, buffer, 255, 0);
+    if (n < 0) {
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+
+    printf("Message from server: %s\n", buffer);
+
+    close(sockfd);
+
+    return 0;
+}
